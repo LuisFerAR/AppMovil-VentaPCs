69bd7ae4d85a8c9460b8c723b1e76b26c7135964
Luis Fernando Aranda Ramos <luis.fernando.aranda.ramos@gmail.com>
2023-06-24 01:56:17 -0500

    ggg

--- app/src/main/java/com/example/proyecto/actividades/RegistroActivity.java
+++ app/src/main/java/com/example/proyecto/actividades/RegistroActivity.java
@@ -38,9 +38,9 @@ public class RegistroActivity extends AppCompatActivity implements View.OnClickL
     RadioGroup ragSexo;
 
     //private final String getController = "https://appmovilxdd.000webhostapp.com/webService/mostrarController.php";
-    private final String setCliente = "https://appmovilxdd.000webhostapp.com/webService/agregarCliente.php";
+    //private final String setCliente = "https://appmovilxdd.000webhostapp.com/webService/agregarCliente.php";
     private final String getController = "http://proyecto-yugioh.atwebpages.com/webServices/mostrarController.php";
-    //private final String setCliente = "http://proyecto-yugioh.atwebpages.com/webServices/agregarCliente.php";
+    private final String setCliente = "http://proyecto-yugioh.atwebpages.com/webServices/agregarCliente.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
